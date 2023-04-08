42aff009cfd25f1aa89163e62733048006a6b7ce
Emile <emabish42@gmail.com>
2023-04-08 11:44:21 +0200

    Небольшие правки

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,14 +1,10 @@
 package app.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Timer {
-
     private final Long nanoTime = System.nanoTime();
-
     public Long getTime() {
         return nanoTime;
     }
